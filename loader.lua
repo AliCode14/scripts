7ad07707cda66d9c0202b4c083146a2590238bb1
Ali <106306929+AliCode14@users.noreply.github.com>
2026-02-04 12:17:27 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,8 @@
 local scriptsByPlaceId = {
 	[3978370137] = "https://raw.githubusercontent.com/AliCode14/scripts/refs/heads/main/GpoKaitun.lua",
-	[7465136166] = "https://raw.githubusercontent.com/AliCode14/scripts/refs/heads/main/GpoKaitun.lua",
 	[120135584963579] = "https://raw.githubusercontent.com/AliCode14/scripts/refs/heads/main/DontStealALabubu.lua",
 	[131623223084840] = "https://raw.githubusercontent.com/AliCode14/scripts/refs/heads/main/EscapeTusnamiForBrainrots!.lua",
+	[648454481] = "https://raw.githubusercontent.com/AliCode14/scripts/refs/heads/main/GpoBr.lua",
 }
 
 local placeId = game.PlaceId
