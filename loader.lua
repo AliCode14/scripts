46693f79564f6da348c45db0872db5fb5eaca52d
Ali <106306929+AliCode14@users.noreply.github.com>
2026-02-10 06:11:17 +0100

    Add multiple script URLs for different Place IDs

--- loader.lua
+++ loader.lua
@@ -1,8 +1,11 @@
 local scriptsByPlaceId = {
 	[3978370137] = "https://raw.githubusercontent.com/AliCode14/scripts/refs/heads/main/GpoKaitun.lua",
+	[1730877806] = "https://raw.githubusercontent.com/AliCode14/scripts/refs/heads/main/GpoKaitun.lua",
+	[116480749200627] = "https://raw.githubusercontent.com/AliCode14/scripts/refs/heads/main/GpoKaitun.lua",
+	[6811831486] = "https://raw.githubusercontent.com/AliCode14/scripts/refs/heads/main/GpoKaitun.lua",
+	[7465136166] = "https://raw.githubusercontent.com/AliCode14/scripts/refs/heads/main/GpoKaitun.lua",
 	[120135584963579] = "https://raw.githubusercontent.com/AliCode14/scripts/refs/heads/main/DontStealALabubu.lua",
 	[131623223084840] = "https://raw.githubusercontent.com/AliCode14/scripts/refs/heads/main/EscapeTusnamiForBrainrots!.lua",
-	[648454481] = "https://raw.githubusercontent.com/AliCode14/scripts/refs/heads/main/GpoBr.lua",
 }
 
 local placeId = game.PlaceId
